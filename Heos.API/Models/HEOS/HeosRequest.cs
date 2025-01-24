a207e683d95eb6534b4b82d6ac52c59ddfb74385
Dominik Marchewicz <marchewiczd@gmail.com>
2025-01-25 00:08:44 +0100

    updated request to take parameters

--- Heos.API/Models/HEOS/HeosRequest.cs
+++ Heos.API/Models/HEOS/HeosRequest.cs
@@ -1,6 +1,31 @@
-﻿namespace Heos.API.Models.HEOS;
+﻿using System.Text;
+using Heos.API.Extensions;
+
+namespace Heos.API.Models.HEOS;
 
 public abstract class HeosRequest
 {
-    public string Command { get; protected init; }
+    protected string Command { get; init; } = string.Empty;
+    protected Dictionary<string, string>? Parameters { get; init; } = null;
+
+    private string GetQueryParams()
+    {
+        var stringBuilder = new StringBuilder("?");
+
+        if (Parameters is null || Parameters.Count == 0)
+        {
+            return string.Empty;
+        }
+
+        foreach (var param in Parameters)
+        {
+            stringBuilder.Append($"{param.Key}={param.Value}&");
+        }
+
+        stringBuilder.RemoveLast();
+
+        return stringBuilder.ToString();
+    }
+
+    public string Build() => Command + GetQueryParams();
 }
