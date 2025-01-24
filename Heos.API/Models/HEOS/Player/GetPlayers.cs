0f11e67e96a593e2b94c6389b73605977c072b4c
Dominik Marchewicz <marchewiczd@gmail.com>
2025-01-25 00:09:09 +0100

    fixed response to be deserializable

--- Heos.API/Models/HEOS/Player/GetPlayers.cs
+++ Heos.API/Models/HEOS/Player/GetPlayers.cs
@@ -1,27 +1,34 @@
-﻿namespace Heos.API.Models.HEOS.Player;
+﻿using Heos.API.JsonConverters;
+using Newtonsoft.Json;
+
+namespace Heos.API.Models.HEOS.Player;
 
 public class GetPlayersRequest : HeosRequest
 {
     public GetPlayersRequest()
     {
         Command = "player/get_players";
+        Parameters = new();
     }
 }
 
+[JsonConverter(typeof(JsonPathConverter))]
 public class GetPlayersResponse : HeosResponse
 {
+    [JsonProperty("payload")]
     public List<GetPlayersResponsePayload>? Payload { get; set; }
 }
 
+[JsonConverter(typeof(JsonPathConverter))]
 public record GetPlayersResponsePayload
 {
-    public string? Name { get; set; }
-    public string? Pid { get; set; }
-    public string? Gid { get; set; }
-    public string? Model { get; set; }
-    public string? Version { get; set; }
-    public string? Network { get; set; }
-    public string? LineOut { get; set; }
-    public string? Control { get; set; }
-    public string? Serial { get; set; }
+    [JsonProperty("name")] public string? Name { get; set; }
+    [JsonProperty("pid")] public string? Pid { get; set; }
+    [JsonProperty("gid")] public string? Gid { get; set; }
+    [JsonProperty("model")] public string? Model { get; set; }
+    [JsonProperty("version")] public string? Version { get; set; }
+    [JsonProperty("network")] public string? Network { get; set; }
+    [JsonProperty("lineout")] public string? LineOut { get; set; }
+    [JsonProperty("control")] public string? Control { get; set; }
+    [JsonProperty("serial")] public string? Serial { get; set; }
 }
