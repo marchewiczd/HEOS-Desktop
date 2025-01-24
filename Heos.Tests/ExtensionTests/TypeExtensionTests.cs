1bc09b8ece316aa9c57f6509f00f13282dd55376
Dominik Marchewicz <marchewiczd@gmail.com>
2025-01-24 23:42:21 +0100

    Added tests for extensions

---
+++ Heos.Tests/ExtensionTests/TypeExtensionTests.cs
@@ -0,0 +1,28 @@
+﻿using Heos.API.Attributes;
+using Heos.API.Extensions;
+
+namespace Heos.Tests.ExtensionTests;
+
+public class TypeExtensionTests
+{
+    [Test]
+    public void GetPropertiesWithAttribute_ReturnsOnlyCorrectProperties()
+    {
+        string[] expected = ["Bar1", "Bar2"];
+        var foo = new Foo();
+        var props =
+            foo.GetType().GetPropertiesWithAttribute(typeof(ResponseFieldAttribute));
+
+        var result = props.Select(info => info.Name).ToArray();
+
+        Assert.That(expected, Is.EquivalentTo(result));
+    }
+}
+
+internal class Foo
+{
+    [ResponseField] public object Bar1 { get; set; } = null!;
+    [ResponseField] public object Bar2 { get; set; } = null!;
+    public object Bar3 { get; set; } = null!;
+    public object Bar4 { get; set; } = null!;
+}
