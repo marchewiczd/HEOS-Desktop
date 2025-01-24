51427d5e5074d885b1cffd19df75bd2a4cc5ea9e
Dominik Marchewicz <marchewiczd@gmail.com>
2025-01-25 00:10:55 +0100

    cleaned generator request models

--- Heos.Tools.RCG/Models/Request.cs
+++ Heos.Tools.RCG/Models/Request.cs
@@ -10,37 +10,15 @@ public class Request
     {
         Endpoint = endpoint;
         Description = description;
-        _parameters = new();
+        _parameters = [];
     }
 
     public string Endpoint { get; private set; }
 
     public string Description { get; private set; }
 
-    public string GetRequestString() =>
-        $"heos://{Endpoint}{GetQueryString()}";
-
-    public void UpdateDescription(string description) =>
-        Description = description;
-
-    public void AppendEpFragment(string fragment) =>
-        Endpoint += $"/{fragment}";
-
     public void AddParameter(RequestParameter parameter) =>
         _parameters.Add(parameter);
 
     public List<RequestParameter> GetParameters() => _parameters;
-
-    private string GetQueryString()
-    {
-        if (_parameters is null || _parameters.Count == 0)
-            return "";
-
-        var sb = new StringBuilder();
-        sb.Append('?');
-
-        _parameters.ForEach(x => sb.Append($"{x.Name.ToLowerInvariant()}={x.Value.ToLowerInvariant()}"));
-
-        return sb.ToString();
-    }
 }
