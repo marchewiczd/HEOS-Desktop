a8560301b33c320a56161b86a50ec137c202faf2
Dominik Marchewicz <marchewiczd@gmail.com>
2025-01-24 23:46:49 +0100

    Added extension to string

---
+++ Heos.API/Extensions/StringBuilderExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+
+namespace Heos.API.Extensions;
+
+public static class StringBuilderExtensions
+{
+    public static StringBuilder RemoveLast(this StringBuilder stringBuilder)
+    {
+        return stringBuilder.Remove(stringBuilder.Length - 1, 1);
+    }
+}
